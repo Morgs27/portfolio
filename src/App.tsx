8cf4ddd413728853b1d38bc15a6f3939fbfb7d43
Morgan Daniel! <morganjdaniel@gmail.com>
2023-03-13 23:00:29 +0000

    landing page initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/App.scss'
+
+import Lottie from 'react-lottie-player'
+import scroll from './scroll.json';
+
+import { FaGithub, FaLinkedin} from 'react-icons/fa';
+import {GrMail} from 'react-icons/gr'
+import {GiAbstract030} from 'react-icons/gi'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className="landing-page">
+
+        <div className="header">
+          <div className="icon spin-hover">
+            <GiAbstract030></GiAbstract030>
+          </div>
+          <div className="navigation">
+
+            <a href="" className="nav-link strike-hover">
+              About
+            </a>
+            <a href="" className="nav-link strike-hover">
+              Projects
+            </a>
+            <a href="" className="nav-link strike-hover">
+              Contact
+            </a>
+
+          </div>
+        </div>
+
+        <div className="content">
+
+          <div className="text">
+            <div className="title">Morgan Daniel</div>
+            <div className="sub-title">FULL-STACK DEVELOPER</div>
+            <div className="buttons">
+
+              <a href="" className="button float-hover">
+                <FaGithub></FaGithub>
+              </a>
+              <a href="" className="button float-hover">
+                <FaLinkedin></FaLinkedin>
+              </a>
+              <a href="" className="button float-hover">
+               <GrMail></GrMail>
+              </a>
+
+
+            </div>
+          </div>
+
+          <div className="cube-container">
+            <div className="cube"></div>
+          </div>
+
+        </div>
+
+        <div className="footer">
+
+          <Lottie 
+          animationData = {scroll}
+          loop
+          play
+          className='footer-animation'
+          />
+
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
