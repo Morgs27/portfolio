0fad942928069de0a8af78a28a4f41c2fd7d0c81
Morgan Daniel! <morganjdaniel@gmail.com>
2023-03-15 04:16:56 +0000

    vh-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,26 @@ import frame from './assets/animations/frame.json';
 import { FaGithub, FaLinkedin} from 'react-icons/fa';
 import {GrMail} from 'react-icons/gr'
 import {GiAbstract030} from 'react-icons/gi'
+import {FaConnectdevelop} from 'react-icons/fa'
+import {BsHexagon} from 'react-icons/bs';
 
 import initCube from './assets/components/Cube/Cube';
 
 import addObservers from './assets/scripts/intersection-observers';
-
+import fixVh from './assets/scripts/fixVh';
 
 function App() {
 
   useEffect(() => {
 
+    // Add a `--vh` variable to account for mobile browsers
+    fixVh();
+
+    // Initialise Cube
     let cubeContainer = document.getElementById('cube-container')
     initCube(cubeContainer)
 
+    // Add Intersection Observers to elements
     addObservers();
    
   }, [])
@@ -31,21 +38,23 @@ function App() {
     
     <div className="App">
       
-      <div className="landing-page">
+      <div className="landing-page page">
 
         <div className="header">
-          <div className="icon spin-hover observe fade down">
-            <GiAbstract030></GiAbstract030>
-          </div>
+          <a href = "" className="icon spin-hover observe fade down" >
+            {/* <GiAbstract030></GiAbstract030> */}
+            {/* <FaConnectdevelop></FaConnectdevelop> */}
+            <BsHexagon></BsHexagon>
+          </a>
           <div className="navigation">
 
-            <a href="" className="nav-link strike-hover observe fade down delay-15 duration-10">
+            <a href="" className="nav-link strike-hover observe fade down delay-16 duration-10">
               About
             </a>
-            <a href="" className="nav-link strike-hover observe fade down delay-18 duration-10">
+            <a href="" className="nav-link strike-hover observe fade down delay-19 duration-10">
               Projects
             </a>
-            <a href="" className="nav-link strike-hover observe fade down delay-21 duration-10">
+            <a href="" className="nav-link strike-hover observe fade down delay-22 duration-10">
               Contact
             </a>
 
@@ -56,16 +65,16 @@ function App() {
 
           <div className="text">
             <div className="title observe fade">Morgan Daniel</div>
-            <div className="sub-title observe fade delay-8">FULL-STACK DEVELOPER</div>
+            <div className="sub-title observe fade delay-3">FULL-STACK DEVELOPER</div>
             <div className="buttons">
 
-              <a href="" className="button float-hover observe fade delay-2 duration-15">
+              <a href="" className="button float-hover observe fade delay-8 duration-15">
                 <FaGithub></FaGithub>
               </a>
-              <a href="" className="button float-hover observe fade delay-3 duration-15">
+              <a href="" className="button float-hover observe fade delay-9 duration-15">
                 <FaLinkedin></FaLinkedin>
               </a>
-              <a href="" className="button float-hover observe fade delay-6 duration-15">
+              <a href="" className="button float-hover observe fade delay-10 duration-15">
                <GrMail></GrMail>
               </a>
 
@@ -74,7 +83,7 @@ function App() {
           </div>
 
           <div className="cube-container">
-            <div className="cube observe fade down delay-30 duration-10" id = 'cube-container' >
+            <div className="cube observe fade down delay-16 duration-10" id = 'cube-container' >
               <Lottie 
               animationData = {frame}
               play
@@ -91,16 +100,18 @@ function App() {
           animationData = {scroll}
           loop
           play
-          className='footer-animation observe fade down delay-30'
+          className='footer-animation observe fade down delay-16'
           />
 
         </div>
 
       </div>
 
-      <div className="about-us-page">
+      <div className="about-us-page page">
 
       </div>
+
+
 
     </div>
 
